49f9291a365213c72dbbaaed4e72df645d0a76e9
soulnov <51446381+soulnov@users.noreply.github.com>
2019-06-06 22:20:39 +0700

    Add files via upload

---
+++ Anime List.js
@@ -0,0 +1,57 @@
+/* CSS Anime List dengan Gambar */
+#recomanim {
+    margin: 1% 1% 0px;
+}
+.stat {
+    position: absolute;
+    z-index: 10;
+    color: #FFF;
+    padding: 3px 6px;
+    font-size: 10px;
+    border-radius: 3px;
+    top: 5px;
+    right: 5px;
+}
+.recobox {
+    margin-bottom: 20px;
+    padding: 5px;
+    background-image: linear-gradient(to bottom, #E8E8E8 0px, #F5F5F5 100%);
+    background-repeat: repeat-x;
+    border: 1px solid #DBDBDB;
+    box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.05) inset, 0px 1px 0px rgba(255, 255, 255, 0.1);
+}
+.anim {
+    float: left;
+    margin: 5px;
+    border: 1px solid #CCC;
+    border-radius: 3px;
+    position: relative;
+    width: 122px;
+    height: 171px;
+    overflow: hidden;
+}
+.stat.ongo {
+    background: none repeat scroll 0% 0% #3B5CA3;
+}
+.anim a {
+    display: block;
+    font-size: 0px;
+}
+.anim img {
+    width: 100%;
+    height: 100%;
+}
+.anim p {
+    margin: 0px;
+    font-size: 11px;
+    background: none repeat scroll 0% 0% rgba(0, 0, 0, 0.5);
+    color: #FFF;
+    position: absolute;
+    bottom: 0px;
+    z-index: 200;
+    padding: 4px 6px 5px;
+    width: 110px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+} 
